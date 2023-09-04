8d5fc526c8915449354ab75db6ea68cd4ec39ad2
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-04 09:58:35 -0500

    restructurando proyecto

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,4 @@
-import { createPool } from 'mysql2'
+import { createPool } from 'mysql2/promise'
 
 // TODO: Creando la conecxión
 export const connection = createPool({
--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
+import { connection } from './db.js'
 
 const app = express()
 const PORT = 3000
+
+// TODO: Trae todos los datos en la base de datos en un objeto JSON
+app.get('/ping', async (req, res) => {
+  const [result] = await connection.query('SELECT * FROM personayumbo')
+  res.json(result)
+})
 
 app.get('/users', (req, res) => {
   res.send('obteniendo los usuarios')
