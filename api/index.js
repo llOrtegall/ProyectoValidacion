53213a3d58be7fd756a9ccdefe3d1c84d32f41dd
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-05 09:48:05 -0500

    rutas principales

--- api/index.js
+++ api/index.js
@@ -1,2 +1,22 @@
-import express from 'express';
+import express from 'express'
 
+const app = express()
+const PORT = 3000
+
+app.get('/users', (req, res) => {
+  res.send('obteniendo los usuarios')
+})
+
+app.post('/users', (req, res) => {
+  res.send('Creando empleados')
+})
+
+app.put('/users', (req, res) => {
+  res.send('actualizando empleados')
+})
+
+app.delete('/users', (req, res) => {
+  res.send('elimando empleados')
+})
+
+app.listen(PORT)
