154f5140d80b696c5e521cfdb862cdb526eb60f9
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-09-06 14:15:25 -0500

    ambas solicitudes funcionando correctamente

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import { routerUser } from './routes/users.routes.js'
-import { routerCliente } from './routes/cliente.routes.js'
+import { routerCliente } from './routes/clientes.routes.js'
 
 const app = express()
 const PORT = 3000
 
+// TODO: routerUser para consultar la bd chatboot
 app.use(routerUser)
+// TODO: routerUser para consultar la bd Produccion
 app.use(routerCliente)
 
-app.listen(PORT)
+app.listen(PORT, () => {
+  console.log(`Server On Port http://localhost:${PORT}`)
+})
