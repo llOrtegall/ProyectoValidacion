457f0f133c7a5089ab1c7cc89eb43457611790d2
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-13 16:01:32 -0500

    agregando cambios api

--- api/src/controllers/userCreated.js
+++ api/src/controllers/userCreated.js
@@ -23,9 +23,10 @@ export const CreateUserclient = async (req, res) => {
 
   const user = req.body;
 
-  console.log(user);
-
   try {
+
+    const { nombre, nombre2, apellido, apellido2, documento, celular } = user
+
     const result = await conecOracDB.execute(`INSERT INTO gamble.clientes 
     (DOCUMENTO, TOTALPUNTOS, USUARIO, FECHASYS, NOMBRES, APELLIDO1, APELLIDO2, FECHANACIMIENTO, 
     TELEFONO, DIRECCION, TIPO_DEPTO, CODDEPTO,TIPO_MUNICIPIO, CODMUNICIPIO, ENT_SEXO, DAT_DTO_SEXO, 
@@ -33,15 +34,14 @@ export const CreateUserclient = async (req, res) => {
     ACEPTAPOLITICATDP, CLIENTEVENDEDOR, CLAVECANAL, TPOTRT_CODIGO_NACION, TRT_CODIGO_NACION, TPOTRT_CODIGO_EXPDOC, TRT_CODIGO_EXPDOC, 
     FECHAEXPDOC, DTO_CODIGO_TPDOC, ENT_CODIGO_TPDOC, IDLOGIN, SECURITY_TOKEN) 
     VALUES 
-    ('41234123', '0000', 'JBOSS', to_date('27/06/18','DD/MM/RR'), 'Ivan', 'Ortega', 'Garzon', to_date('27/01/81','DD/MM/RR'),
-    '3202191681', '', '6', '30', '8', '965', '60', '33', '66910151', '00000', '0' ,'0', '', null, null, null, null, null, '101010', 
-    null, null, null, null, null, null, null, null, null)`);
+    ('${documento}', '0000', 'JBOSS', to_date('27/06/18','DD/MM/RR'), '${nombre}', '${apellido}', '${apellido2}', to_date('27/01/81','DD/MM/RR'),
+    '${celular}', '', '6', '30', '8', '965', '60', '33', '66910151', '00000', '0' ,'0', '', null, null, null, null, null, '101010', null, null, null, null, null, null, null, null, null)`);
 
     await conecOracDB.commit();
     res.status(201).json({ 'success': true, 'message': 'Commit successfully committed' })
 
   } catch (error) {
-    res.status(404).json({ 'success': false, 'message': 'Commit error committed' })
+    res.status(500).json({ 'success': false, 'message': 'Commit error committed' })
     console.error('Error al insertar datos:', error);
   }
 }
