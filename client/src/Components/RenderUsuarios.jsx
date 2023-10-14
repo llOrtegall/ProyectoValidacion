62c37d6e3aacfd4c17170da41ed0ec831907e1ef
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-14 10:00:37 -0500

    verificar

--- client/src/Components/RenderUsuarios.jsx
+++ client/src/Components/RenderUsuarios.jsx
@@ -14,9 +14,34 @@ export function RenderUsuarios() {
   }, [])
 
   return (
-    <section>
-      {user.length > 0
-        ? user.map()}
+    <section className="w-full flex flex-col p-2">
+      <h1 className="p-3 text-white rounded-t-xl text-xl font-semibold bg-blue-500 text-center">Usuarios Registrados Por Chat Boot</h1>
+      <table className="">
+        <thead >
+          <tr>
+            <th>Nombres</th>
+            <th>Documento</th>
+            <th>Telefono</th>
+            <th>Correo</th>
+            <th>N° Registro</th>
+          </tr>
+        </thead>
+        <tbody className="text-center">
+          {console.log(user)}
+          {user.map(item => {
+            return (
+              <tr key={item.cedula}>
+                <td>{item.nombre}</td>
+                <td>{item.cedula}</td>
+                <td>{item.telefono}</td>
+                <td>{item.correo}</td>
+                <td>{item.telwhats}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table >
     </section>
+
   )
 }
