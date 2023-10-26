99c7aab40e9c4a1487ed468122c9852fe4f70ae9
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-10-26 12:48:23 -0500

    funciona ok sigue

--- client/src/Components/InfoUsuario.jsx
+++ client/src/Components/InfoUsuario.jsx
@@ -1,25 +1,27 @@
-import { InfoIcon } from "./IconsSvg";
+import { CloseIcon, InfoIcon } from "./IconsSvg";
 
 // eslint-disable-next-line react/prop-types
 export function InfoUsuario({ inf, fun }) {
 
   const toggleComponent = fun
 
-
   return (
     <>
       <section className="flex w-auto m-2 p-2 bg-yellow-300 rounded-lg justify-center ">
         <span className="font-semibold">Por Favor Antes De Agregar El Usuario Validar Con Alguna Página Del Estado La Información Recibida</span>
       </section>
-      <section className="flex m-2 p-6 bg-zinc-300 rounded-lg items-center">
-        <figure className="w-auto h-full pr-2 ">
-          <InfoIcon />
-        </figure>
-        <div className="flex flex-col border rounded-md p-6 bg-zinc-400 w-auto">
+
+      <section className="relative m-2 p-6 bg-zinc-300 rounded-lg ">
+
+        <div className="flex">
+          <figure className="flex items-center pr-2 ">
+            <InfoIcon />
+          </figure>
+
           {inf.length > 0
             ? inf.map(
               i => (
-                <div key={i.cedula}>
+                <div key={i.cedula} className="m-2 p-2">
                   <div className="flex justify-between">
                     <h1 className="font-bold pr-8">Nombres: </h1>
                     <h1 className="">{i.nombre}</h1>
@@ -47,15 +49,17 @@ export function InfoUsuario({ inf, fun }) {
                 </div>
               ))
             : null}
-        </div>
-        <article className="grid grid-cols-1 w-auto h-auto">
-          <button className="hover:text-white shadow-lg font-semibold p-2 mx-4 my-2 rounded-xl bg-green-400">Crear Usuario</button>
-          <button className="hover:text-white shadow-lg font-semibold p-2 mx-4 my-2 rounded-xl bg-yellow-400">Editar Usuario</button>
-          <button className="hover:text-white shadow-lg font-semibold p-2 mx-4 my-2 rounded-xl bg-purple-400">Validar Usuario</button>
-        </article>
-        <button onClick={toggleComponent}>cerrar
 
-        </button>
+          <aside className="flex flex-col">
+            <button className="hover:text-white shadow-lg font-semibold p-2 mx-4 my-2 rounded-xl bg-green-400">Crear Usuario</button>
+            <button className="hover:text-white shadow-lg font-semibold p-2 mx-4 my-2 rounded-xl bg-yellow-400">Editar Usuario</button>
+            <button className="hover:text-white shadow-lg font-semibold p-2 mx-4 my-2 rounded-xl bg-purple-400">Validar Usuario</button>
+          </aside>
+
+        </div>
+
+        <button className="absolute top-2 right-2 hover:bg-red-700 hover:rounded-full hover:text-white" onClick={toggleComponent}><CloseIcon /></button>
+
       </section >
     </>
   )
