7cfd29002045f992adef699e571814998889ad30
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-11-01 09:04:47 -0500

    punto de retorno 01/11/2023

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,16 @@ import { AuthContext } from './auth/AuthContext'
 import { Dashboard } from './routes/Dashboard'
 
 export function App () {
+  // Configuración de axios
   axios.defaults.baseURL = 'http://localhost:3000'
   axios.defaults.withCredentials = true
 
+  // Obtener el usuario actual del contexto de autenticación
   const { user } = useContext(AuthContext)
 
+  // Si el usuario está autenticado, mostrar el panel de control
   if (user.usuario !== null) return <Dashboard />
 
+  // Si el usuario no está autenticado, mostrar la página de inicio de sesión
   return <Login />
 }
