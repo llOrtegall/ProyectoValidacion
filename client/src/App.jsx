551076f85ca3de5ab445332154dbbbb095ed2dea
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-11-09 11:33:18 -0500

    cambios realizados para funcionar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from './auth/AuthContext'
 import { Dashboard } from './routes/Dashboard'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://172.20.1.191:3000'
+axios.defaults.baseURL = 'http://localhost:3000'
 axios.defaults.withCredentials = true
 
 export function App () {
