e0b386af472b8b31c151c00074521341f09ea7fa
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-11-15 08:48:01 -0500

    quitando api ya que esta en el servidor

--- api/src/routes/clienteFiel.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { createdClientFiel, getClientFiel } from '../controllers/clienteFiel.controller.js'
-
-export const routerCF = Router()
-
-routerCF.post('/getCF', getClientFiel)
-
-routerCF.post('/newCF', createdClientFiel)
