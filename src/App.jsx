eeeaa7fd00293270e8535fd4af8866c0bdb2888f
llOrtegall <IvanOrtega_97@hotmail.com>
2023-12-23 11:20:27 -0500

    tailwind agregado ok

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ export function App() {
         <Route path="home" element={< Home />}></Route>
         <Route path="items" element={< Items />}></Route>
       </Route>
+      <Route path="*" element={<h1>Not Found</h1>}></Route>
     </Routes>
   )
 }
