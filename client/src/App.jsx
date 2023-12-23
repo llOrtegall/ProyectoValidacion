8fdd219a9618652cdeb661c04248cc396792a757
llOrtegall <IvanOrtega_97@hotmail.com>
2023-12-23 11:07:50 -0500

    desde solo el front

--- client/src/App.jsx
+++
@@ -1,7 +0,0 @@
-export function App () {
-  return (
-    <main className="h-screen w-screen bg-blue-300">
-      <h1>Hello World</h1>
-    </main>
-  )
-}
--- client/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-  './src/**/*.{html,jsx,tsx}'
-]
-export const theme = {
-  extend: {}
-}
-export const plugins = []
