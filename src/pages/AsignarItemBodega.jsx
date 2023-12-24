d9aab8889c7b6c8a6476e1538d2568a82a956e25
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-12-24 13:28:16 -0500

    continuar inplementando asignacipon

--- src/pages/AsignarItemBodega.jsx
+++ src/pages/AsignarItemBodega.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 
 export function AsignarItemBodega() {
   const [bodegas, setBodegas] = useState([])
+  const [items, setItems] = useState([])
 
   useEffect(() => {
     axios.get('/getBodegas')
@@ -14,21 +15,48 @@ export function AsignarItemBodega() {
       })
   }, [])
 
-  return (
-<main className="bg-slate-100">
-  <h1>Asignar Item Bodega</h1>
-  <select className="bg-white shadow-lg p-2">
-
-    {
-      bodegas.map((bodega) => {
-        return (
-          <option value={bodega._id} key={bodega._id}>
-            {bodega.nombre} - {bodega.sucursal} - {bodega.direccion}
-          </option>
-        )
+  useEffect(() => {
+    axios.get('/getItems')
+      .then((response) => {
+        console.log(response.data);
+        setItems(response.data)
       })
-    }
-  </select>
-</main>
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  return (
+    <main className="flex flex-col items-center w-full h-screenbg-slate-100">
+      <h1 className="text-2xl py-4 ">Asignar Item Bodega</h1>
+
+      <form className="flex gap-2">
+        <select className="bg-slate-400 rounded-md shadow-lg p-2">
+          <option value="">Seleccione un item para asignar</option>
+          {
+            items.map((item) => {
+              return (
+                <option key={item._id} value={item._id} >
+                  {item.nombre} - {item.placa} - {item.serial}
+                </option>
+              )
+            })
+          }
+        </select>
+
+        <select className="bg-slate-400 rounded-md shadow-lg p-2">
+          <option value="">Seleccione una bodega</option>
+          {
+            bodegas.map((bodega) => {
+              return (
+                <option key={bodega._id} value={bodega.sucursal} >
+                  {bodega.nombre} - {bodega.sucursal} - {bodega.direccion}
+                </option>
+              )
+            })
+          }
+        </select>
+      </form>
+    </main>
   )
 }
