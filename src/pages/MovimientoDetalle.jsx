349614be668616e461fc17853bec53936ea4abce
llOrtegall <IvanOrtega_97@hotmail.com>
2024-01-03 10:05:41 -0500

    works

--- src/pages/MovimientoDetalle.jsx
+++ src/pages/MovimientoDetalle.jsx
@@ -19,7 +19,7 @@ export function MovimientoDetalle () {
     return <div>Loading...</div>
   }
 
-  const { bodegaDestino, bodegaOrigen, encargado, fecha, incidente, movimientoId, items } = movimiento
+  const { bodegaDestino, bodegaOrigen, encargado, fecha, incidente, movimientoId, items, descripcion } = movimiento
 
   return (
 
@@ -32,17 +32,19 @@ export function MovimientoDetalle () {
           </h1>
         </section>
 
-        <section className='grid grid-cols-2 place-items-center p-2'>
+        <section className='grid grid-cols-2 p-2'>
           <p className="text-blue-800 font-medium">Fecha: <span className='font-semibold text-black'>{formatFecha(fecha)}</span></p>
+          <p className="text-blue-800 font-medium">Nombre Bodega Origen: <span className='font-semibold text-black'>{bodegaOrigen.nombre}</span></p>
           <p className="text-blue-800 font-medium">Encargado: <span className='font-semibold text-black'>{encargado}</span></p>
+          <p className="text-blue-800 font-medium">Sucursal Origen: <span className='font-semibold text-black'>{bodegaOrigen.sucursal}</span> </p>
           <p className="text-blue-800 font-medium">Incidente: <span className='font-semibold text-black'>{incidente}</span></p>
-          <p className="text-blue-800 font-medium">Bodega Origen:<span className='font-semibold text-black'>{bodegaOrigen.nombre}</span></p>
+          <p className="text-blue-800 font-medium">Nombre Bodega Destino: <span className='font-semibold text-black'>{bodegaDestino.nombre}</span> </p>
           <p className="text-blue-800 font-medium">Cantidad De Items Movidos: <span className='font-semibold text-black'>{items.length}</span> </p>
-          <p className="text-blue-800 font-medium">Bodega Destino: <span className='font-semibold text-black'>{bodegaDestino.nombre}</span> </p>
+          <p className="text-blue-800 font-medium">Sucursal Destino: <span className='font-semibold text-black'>{bodegaDestino.sucursal}</span> </p>
         </section>
       </article>
 
-      <article className='w-full'>
+      <article className='w-full flex flex-col gap-2 '>
         <table className="table-auto w-full">
           <thead>
             <tr className='bg-yellow-100'>
@@ -67,6 +69,12 @@ export function MovimientoDetalle () {
             }
           </tbody>
         </table>
+        <section className='border border-black h-auto '>
+          <header>
+            <h3 className='p-2 bg-yellow-100 border-b-2 border-black text-center font-medium'>Motivo / Descripción Movimiento</h3>
+          </header>
+          <p className='py-2 pl-1'>{descripcion}</p>
+        </section>
       </article>
 
     </section>
