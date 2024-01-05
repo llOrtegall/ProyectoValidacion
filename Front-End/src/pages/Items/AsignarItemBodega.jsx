6ca25cdc978aca8d6995da5017f2886e55661e8e
llOrtegall <IvanOrtega_97@hotmail.com>
2024-01-05 13:11:49 -0500

    changes ok organizando stilos

--- Front-End/src/pages/Items/AsignarItemBodega.jsx
+++ Front-End/src/pages/Items/AsignarItemBodega.jsx
@@ -60,78 +60,79 @@ export function AsignarItemBodega () {
   }
 
   return (
-    <main className="w-full flex justify-around flex-col">
+    <main className="w-full h-[93vh] flex justify-around">
 
-      <section className='flex justify-around'>
-        <article className="flex flex-col gap-4 items-center">
-          <section className='flex items-center pt-10 gap-5'>
-            <p className=""><span className="font-semibold pr-2">Filtrar:</span>| Placa | Serial | Nombre |</p>
-            <input type="text" placeholder="Buscar Items..."
-              value={search} onChange={ev => setSearch(ev.target.value)}
-              className="bg-slate-200 w-64 p-2 rounded-md" />
-          </section>
+      <article className="w-[550px]">
 
-          <section name="itemIds"
-            className="bg-slate-200 rounded-md shadow-lg p-2 min-w-96 flex flex-col gap-2">
-            {
-              filteredItems.map(item => (
-                item.bodega === 'No Asignado' && (
-                  <article key={item._id} value={item._id} className='flex justify-between bg-slate-300 px-2 py-1 rounded-md hover:bg-blue-200'>
-                    {item.placa} | {item.nombre}
-                    <button
-                      onClick={() => handleAddItem(item._id)}
-                      className={carItems.includes(item._id) ? 'added' : 'hover:bg-green-300 hover:rounded-full'}
-                    >
-                      <AddIcon />
-                    </button>
-                  </article>
-                )
+        <section className='flex items-center gap-4 py-6'>
+          <p className=""><span className="font-semibold pr-2">Filtrar:</span>| Placa | Serial | Nombre |</p>
+          <input type="text" placeholder="Buscar Items..."
+            value={search} onChange={ev => setSearch(ev.target.value)}
+            className="bg-slate-200 w-64 p-2 rounded-md" />
+        </section>
+
+        <section name="itemIds"
+          className="bg-slate-200 rounded-md shadow-lg p-2 min-w-96 flex flex-col gap-2">
+          {
+            filteredItems.map(item => (
+              item.bodega === 'No Asignado' && (
+                <article key={item._id} value={item._id} className='flex justify-between bg-slate-300 px-2 py-1 rounded-md hover:bg-blue-200'>
+                  {item.placa} | {item.nombre}
+                  <button
+                    onClick={() => handleAddItem(item._id)}
+                    className={carItems.includes(item._id) ? 'added' : 'hover:bg-green-300 hover:rounded-full'}
+                  >
+                    <AddIcon />
+                  </button>
+                </article>
               )
-              )
-            }
-          </section>
+            )
+            )
+          }
+        </section>
 
-          <section className='w-full flex flex-col'>
-            <h2 className='text-xl font-semibold text-center pb-4'>Items Seleccionados Para Asignación: </h2>
-            {
-              carItems && (
-                carItems?.map(item => (
-                  <ItemsAgregados id={item} key={item} items={itemsConBodega} handleRemoveItem={handleRemoveItem} />
-                ))
-              )
-            }
-          </section>
-        </article>
-
-        <article className="flex flex-col gap-4 items-center">
-          <section className='flex items-center pt-10 gap-5'>
-            <p className=""><span className="font-semibold pr-2">Filtrar:</span>| Sucursal | Nombre | Dirección </p>
-            <input type="text" placeholder="Buscar bodega..."
-              value={searchBodega} onChange={ev => setSearchBodega(ev.target.value)}
-              className="bg-slate-200 w-64 p-2 rounded-md" />
-          </section>
-          <select name="sucursal" id="sucursal"
-            value={sendBodega} onChange={ev => setSendBodega(ev.target.value)}
-            className="bg-slate-300 rounded-md shadow-lg p-2 min-w-96">
-            <option value="">Seleccione una bodega</option>
-            {
-              filteredBodegas.map(bodega => (
-                <option key={bodega._id} value={bodega.sucursal} className='justify-normal'>
-                  {bodega.sucursal} | {bodega.nombre}
-                </option>
+        <section style={{ maxHeight: '450px', overflowY: 'auto' }}>
+          <h2 className='text-xl font-semibold text-center'>Items Seleccionados Para Asignación: </h2>
+          {
+            carItems && (
+              carItems?.map(item => (
+                <ItemsAgregados id={item} key={item} items={itemsConBodega} handleRemoveItem={handleRemoveItem} />
               ))
-            }
-          </select>
+            )
+          }
+        </section>
+      </article>
 
-          <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center">
-            <button className="w-60 h-10 bg-blue-400 hover:bg-blue-600 rounded-lg text-white font-semibold">
-              Asignar
-            </button>
-          </form>
-        </article>
-      </section>
+      <article className="w-[550px]">
 
-      <MessageDisplay message={message} error={error} />
+        <section className='flex items-center gap-4 py-6'>
+          <p className=""><span className="font-semibold pr-2">Filtrar:</span>| Sucursal | Nombre | Dirección </p>
+          <input type="text" placeholder="Buscar bodega..."
+            value={searchBodega} onChange={ev => setSearchBodega(ev.target.value)}
+            className="bg-slate-200 w-64 p-2 rounded-md" />
+        </section>
+
+        <select className="bg-slate-200 rounded-md shadow-lg p-2 w-full flex flex-col gap-2 mb-4"
+          value={sendBodega} onChange={ev => setSendBodega(ev.target.value)}
+          name="sucursal" id="sucursal">
+          <option value="">Seleccione una bodega</option>
+          {
+            filteredBodegas.map(bodega => (
+              <option key={bodega._id} value={bodega.sucursal} className='justify-normal'>
+                {bodega.sucursal} | {bodega.nombre}
+              </option>
+            ))
+          }
+        </select>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center mb-4">
+          <button className="w-60 h-10 bg-blue-400 hover:bg-blue-600 rounded-lg text-white font-semibold">
+            Asignar
+          </button>
+        </form>
+
+        <MessageDisplay message={message} error={error} />
+      </article>
     </main>
   )
 }
