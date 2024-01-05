7c5bd2f62bdc62eadcc93bbf935f7322c68e7c51
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-01-04 19:29:13 -0500

    vamos bien

--- Front-End/src/pages/AsignarItemBodega.jsx
+++ Front-End/src/pages/AsignarItemBodega.jsx
@@ -1,22 +1,36 @@
-import { fechtItemsBodegas } from '../utils/FetchItemsData.js'
+import { BodegaData, fechtItemsBodegas } from '../utils/FetchItemsData.js'
 import { MessageDisplay } from '../components/MessageDisplay.jsx'
 import { ItemsAgregados } from '../components/ItemsAgregados.jsx'
-// import { useCarItems } from '../hooks/useCartItems.js'
+import { useCarItems } from '../hooks/useCartItems.js'
 import { AddIcon } from '../components/Icons.jsx'
 import { useEffect, useState } from 'react'
-import { useFiltersItems } from '../hooks/useFilters.js'
+import { useFiltersBodegas, useFiltersItems } from '../hooks/useFilters.js'
 
-export function AsignarItemBodega () {
+export function AsignarItemBodega() {
   const [itemsConBodega, setItemsConBodega] = useState([])
+  const [bodegas, setBodegas] = useState([])
+
   const [message, setMessage] = useState('')
   const [error, setError] = useState('')
 
   const { search, setSearch, filteredItems } = useFiltersItems(itemsConBodega)
+  const { filteredBodegas, searchBodega, setSearchBodega } = useFiltersBodegas(bodegas)
+  const { handleAddItem, handleRemoveItem, carItems } = useCarItems()
+
+  const [sendBodega, setSendBodega] = useState('')
 
   useEffect(() => {
     fechtItemsBodegas()
       .then(data => {
         setItemsConBodega(data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  useEffect(() => {
+    BodegaData()
+      .then(data => {
+        setBodegas(data)
       })
       .catch(err => console.log(err))
   }, [])
@@ -58,15 +72,15 @@ export function AsignarItemBodega () {
             {
               filteredItems.map(item => (
                 item.bodega === 'N/A' && (
-                <article key={item._id} value={item._id} className='flex justify-between'>
-                  {item.placa} | {item.nombre}
-                  <button
-                    // onClick={() => handleAddItem(item._id)}
-                    // className={carItems.includes(item._id) ? 'added' : ''}
-                  >
-                    <AddIcon />
-                  </button>
-                </article>
+                  <article key={item._id} value={item._id} className='flex justify-between'>
+                    {item.placa} | {item.nombre}
+                    <button
+                      onClick={() => handleAddItem(item._id)}
+                      className={carItems.includes(item._id) ? 'added' : ''}
+                    >
+                      <AddIcon />
+                    </button>
+                  </article>
                 )
               )
               )
@@ -75,13 +89,13 @@ export function AsignarItemBodega () {
 
           <section className='w-full flex flex-col'>
             <h2 className='text-xl font-semibold text-center pb-4'>Items Seleccionados Para Asignación: </h2>
-            {/* {
+            {
               carItems && (
                 carItems?.map(item => (
-                  <ItemsAgregados id={item} key={item} items={items} handleRemoveItem={handleRemoveItem} />
+                  <ItemsAgregados id={item} key={item} items={itemsConBodega} handleRemoveItem={handleRemoveItem} />
                 ))
               )
-            } */}
+            }
           </section>
         </article>
 
@@ -89,20 +103,20 @@ export function AsignarItemBodega () {
           <section className='flex items-center pt-10 gap-5'>
             <p className=""><span className="font-semibold pr-2">Filtrar:</span>| Sucursal | Nombre | Dirección </p>
             <input type="text" placeholder="Buscar bodega..."
-              // value={searchBodega} onChange={ev => setSearchBodega(ev.target.value)}
+              value={searchBodega} onChange={ev => setSearchBodega(ev.target.value)}
               className="bg-slate-200 w-64 p-2 rounded-md" />
           </section>
           <select name="sucursal" id="sucursal"
-            // value={sendBodega} onChange={ev => setSendBodega(ev.target.value)}
+            value={sendBodega} onChange={ev => setSendBodega(ev.target.value)}
             className="bg-slate-300 rounded-md shadow-lg p-2 min-w-96">
             <option value="">Seleccione una bodega</option>
-            {/* {
+            {
               filteredBodegas.map(bodega => (
                 <option key={bodega._id} value={bodega.sucursal} className='justify-normal'>
                   {bodega.sucursal} | {bodega.nombre}
                 </option>
               ))
-            } */}
+            }
           </select>
 
           <form onSubmit={handleSubmit} className="flex flex-col gap-4 items-center">
