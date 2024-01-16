f76b0130eb408a803fc66ac3155ebb1b95d402fe
llOrtegall <IvanOrtega_97@hotmail.com>
2024-01-16 11:10:39 -0500

    works ok

--- Front-End/src/components/ComponenteSimcards.jsx
+++ Front-End/src/components/ComponenteSimcards.jsx
@@ -2,8 +2,16 @@ import { SimcardAgregada } from '../components/SimcardAgregada.jsx'
 
 // eslint-disable-next-line react/prop-types
 export function ComponenteSimcards ({ bodegaOrigen, bodegaDestino, handleRemoveItem, handleRemoveItem2, cartSims, cartSims2 }) {
-  return (
+  return (<>
+
+    <h1 className='text-xl font-semibold text-center py-1 rounded-t-md bg-blue-400'>Movimiento </h1>
+    <header className='flex justify-around bg-yellow-100 p-1 text-black '>
+      <h3> <span className="font-bold">Nombre:</span>  {bodegaDestino?.nombre}</h3>
+      <p> <span className="font-bold">Direccion:</span>  {bodegaDestino?.direccion}</p>
+      <p> <span className="font-bold">N° Sucursal:</span>  {bodegaDestino?.sucursal}</p>
+    </header>
     <section className='grid grid-cols-2'>
+
       <main className='col-span-1'>
         <h2 className="text-center py-1 font-semibold bg-green-200 text-black rounded-t-lg">Simcards Que Ingresarán :</h2>
         <section style={{ maxHeight: '450px', overflowY: 'auto' }}>
@@ -33,5 +41,6 @@ export function ComponenteSimcards ({ bodegaOrigen, bodegaDestino, handleRemoveI
         </section>
       </main>
     </section>
+  </>
   )
 }
--- Front-End/src/pages/Simcards/Movimientos.jsx
+++ Front-End/src/pages/Simcards/Movimientos.jsx
@@ -6,6 +6,7 @@ import axios from 'axios'
 import { ComponenteSimcards } from '../../components/ComponenteSimcards.jsx'
 import { RenderBodegaOrigen } from '../../components/RenderBodegaOrigen.jsx'
 import { RenderBodegaDestino } from '../../components/RenderBodegaDestino.jsx'
+import { FooterMovSim } from '../../components/FooterCompSimcaMov.jsx'
 
 export function Movimientos () {
   const [bodegaDestino, setBodegaDestino] = useState(null)
@@ -58,40 +59,11 @@ export function Movimientos () {
 
       {/* //* Renderizado Movimiento */}
       <article className='mx-2 rounded-md'>
-        <h1 className='text-xl font-semibold text-center py-1 rounded-t-md bg-blue-400'>Movimiento </h1>
-        <header className='flex justify-around bg-yellow-100 p-1 text-black '>
-          <h3> <span className="font-bold">Nombre:</span>  {bodegaDestino?.nombre}</h3>
-          <p> <span className="font-bold">Direccion:</span>  {bodegaDestino?.direccion}</p>
-          <p> <span className="font-bold">N° Sucursal:</span>  {bodegaDestino?.sucursal}</p>
-        </header>
-
         <ComponenteSimcards bodegaOrigen={bodegaOrigen} bodegaDestino={bodegaDestino} cartSims={cartSims}
           cartSims2={cartSims2} handleRemoveItem={handleRemoveItem} handleRemoveItem2={handleRemoveItem2}/>
-
       </article>
 
-      <footer className="py-4 bg-slate-600 rounded-md text-white mx-2 mt-2">
-        <form className="grid grid-cols-2 gap-3">
-          <label className="flex h-10 items-center ml-3"> <span className="font-semibold w-32">Encargado:</span>
-            <input type="text" className="w-full p-2 rounded-md col-span-1 bg-slate-100 no-underline text-black"
-              value={encargado}
-              onChange={ev => setEncargado(ev.target.value)}
-              placeholder="Pepito Perez Muñoz" />
-          </label>
-          <label className="flex h-10 items-center"> <span className="font-semibold w-32">N° Incidente:</span>
-            <input type="text" className="w-full p-2 rounded-md bg-slate-100 no-underline text-black"
-              value={incidente}
-              onChange={ev => setIncidente(ev.target.value)}
-              placeholder="134564 | 234252 | 634532" />
-          </label>
-          <label className="col-span-3 mx-3"> <span className="font-semibold w-40">Observaciones:</span>
-            <input type="text" className="w-full p-2 rounded-md bg-slate-100 no-underline text-black"
-              value={descripcion}
-              onChange={ev => setDescripcion(ev.target.value)}
-              placeholder="texto para registrar observación ..." />
-          </label>
-        </form>
-      </footer>
+      <FooterMovSim encargado={encargado} setEncargado={setEncargado} incidente={incidente} setIncidente={setIncidente} descripcion={descripcion} setDescripcion={setDescripcion}/>
 
       <section className="flex w-full justify-center mt-4">
         <button className="p-2 text-white font-bold w-48 bg-green-600 rounded-md hover:bg-white  hover:text-black" onClick={handleClick}>
