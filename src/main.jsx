cd1ca1a5d0cf5d36bcc7beafceab35ca5f743b3d
llOrtegall <IvanOrtega_97@hotmail.com>
2024-01-23 16:37:21 -0500

    antes de aplicar los hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 import { createRoot } from 'react-dom/client'
-// import { App } from './App.jsx'
-// import './index.css'
+import { App } from './App.jsx'
+import './index.css'
 
-// import { BrowserRouter } from 'react-router-dom'
-// import { AuthProvider } from './Auth/AuthContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './Auth/AuthContext.jsx'
 
-import { App2 } from './source/App2.jsx'
+// import { App2 } from './source/App2.jsx'
+// <App2 />
 
 createRoot(document.getElementById('root')).render(
-  <App2 />
-
-  // <BrowserRouter>
-  //   <AuthProvider>
-  //     <App />
-  //   </AuthProvider>
-  // </BrowserRouter>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 )
