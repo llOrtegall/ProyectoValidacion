db9ff56481f2acb996f7a5071caddd370e932bbb
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-05 11:09:27 -0500

    ver mov ok

--- src/pages/Movimientos/MovimientoDetalle.jsx
+++ src/pages/Movimientos/MovimientoDetalle.jsx
@@ -25,14 +25,14 @@ export function MovimientoDetalle ({ company }) {
 
     movimiento &&
     <main className='flex flex-col gap-4 h-[93vh] px-2'>
-      <article className='border bg-slate-200 mt-4'>
-        <section className='border-t border-l border-r border-black'>
-          <h1 className="text-lg text-center bg-gradient-to-b from-cyan-200 to-blue-300 p-1 font-semibold">Movimiento:
+      <article className='bg-slate-200 mt-4'>
+        <section className='t l r black'>
+          <h1 className="text-lg text-center bg-blue-300 p-1 font-semibold">Movimiento:
             <span className='font-semibold pl-2'>{movimientoId}</span>
           </h1>
         </section>
 
-        <section className='grid grid-cols-2 px-4 py-2 border border-black '>
+        <section className='grid grid-cols-2 px-4 py-2 black '>
           <p className="text-blue-800 font-medium">Fecha: <span className='font-semibold text-black'>{formatFecha(fecha)}</span></p>
           <p className="text-blue-800 font-medium">Nombre Bodega Origen: <span className='font-semibold text-black'>{bodegaOrigen.nombre}</span></p>
           <p className="text-blue-800 font-medium">Encargado: <span className='font-semibold text-black'>{encargado}</span></p>
@@ -45,9 +45,9 @@ export function MovimientoDetalle ({ company }) {
         </section>
       </article>
 
-      <section className='border border-black h-auto '>
+      <section className='black h-auto '>
         <header>
-          <h3 className='p-1 bg-yellow-200 border-black text-center font-medium border-b'>Motivo / Descripción Movimiento</h3>
+          <h3 className='p-1 bg-yellow-200 black text-center font-medium b'>Motivo / Descripción Movimiento</h3>
         </header>
         <p className='py-2 pl-1 bg-slate-200'>{descripcion}</p>
       </section>
@@ -56,11 +56,11 @@ export function MovimientoDetalle ({ company }) {
         <table className="table-auto w-full bg-slate-200">
           <thead >
             <tr className='bg-yellow-200'>
-              <th className="border py-1">Item</th>
-              <th className="border py-1">Descripción</th>
-              <th className='border py-1'>N° Placa</th>
-              <th className="border py-1">Serial</th>
-              <th className="border py-1">Cantidad</th>
+              <th className="bg-blue-300 py-1">Item</th>
+              <th className="bg-blue-300 py-1">Descripción</th>
+              <th className='bg-blue-300 py-1'>N° Placa</th>
+              <th className="bg-blue-300 py-1">Serial</th>
+              <th className="bg-blue-300 py-1">Cantidad</th>
             </tr>
           </thead>
           <tbody>
@@ -68,11 +68,11 @@ export function MovimientoDetalle ({ company }) {
               items.length > 0
                 ? items.map(item => (
                   <tr key={item._id}>
-                    <td className="border text-center">{item.nombre}</td>
-                    <td className="border text-center">{item.descripcion}</td>
-                    <td className='border uppercase text-center'>{item.placa}</td>
-                    <td className='border uppercase text-center'>{item.serial}</td>
-                    <td className="border text-center">1</td>
+                    <td className="text-center">{item.nombre}</td>
+                    <td className="text-center">{item.descripcion}</td>
+                    <td className='uppercase text-center'>{item.placa}</td>
+                    <td className='uppercase text-center'>{item.serial}</td>
+                    <td className="text-center">1</td>
                   </tr>
                 ))
                 : <tr><td colSpan='5' className='text-center'>No Se Realizaron Movimientos De Items</td></tr>
@@ -83,14 +83,14 @@ export function MovimientoDetalle ({ company }) {
 
       <article className='w-full flex flex-col gap-2 bg-slate-200'>
         <table className="table-auto w-full ">
-          <thead >
-            <tr className='bg-yellow-200'>
-              <th className="border py-1">N°</th>
-              <th className="border py-1">Número</th>
-              <th className="border py-1">Operador</th>
-              <th className="border py-1">Serial</th>
-              <th className="border py-1">Estado</th>
-              <th className="border py-1">Movimiento</th>
+          <thead>
+            <tr>
+              <th className="py-1 bg-blue-300">N°</th>
+              <th className="py-1 bg-blue-300">Número</th>
+              <th className="py-1 bg-blue-300">Operador</th>
+              <th className="py-1 bg-blue-300">Serial</th>
+              <th className="py-1 bg-blue-300">Estado</th>
+              <th className="py-1 bg-blue-300">Movimiento</th>
             </tr>
           </thead>
           <tbody>
@@ -98,12 +98,12 @@ export function MovimientoDetalle ({ company }) {
               simcards.entran.length > 0
                 ? simcards.entran.map((sim, index) => (
                   <tr key={sim._id} className='bg-green-200'>
-                    <td className="border text-center">{index + 1}</td>
-                    <td className="border text-center">{sim.numero}</td>
-                    <td className="border text-center">{sim.operador}</td>
-                    <td className='border text-center'>{sim.estado}</td>
-                    <td className='border uppercase text-center'>{sim.serial}</td>
-                    <td className='border text-center'>Entran</td>
+                    <td className="text-center">{index + 1}</td>
+                    <td className="text-center">{sim.numero}</td>
+                    <td className="text-center">{sim.operador}</td>
+                    <td className='text-center'>{sim.estado}</td>
+                    <td className='uppercase text-center'>{sim.serial}</td>
+                    <td className='text-center'>Entran</td>
                   </tr>
                 ))
                 : <tr><td colSpan='5' className='text-center'>No Se Realizaron Entrada De Simcards</td></tr>
@@ -113,12 +113,12 @@ export function MovimientoDetalle ({ company }) {
               simcards.salen.length > 0
                 ? simcards.salen.map((sim, index) => (
                   <tr key={sim._id} className='bg-red-200'>
-                    <td className="border text-center">{index + 1}</td>
-                    <td className="border text-center">{sim.numero}</td>
-                    <td className="border text-center">{sim.operador}</td>
-                    <td className='border text-center'>{sim.estado}</td>
-                    <td className='border uppercase text-center'>{sim.serial}</td>
-                    <td className='border text-center'>Salen</td>
+                    <td className="text-center">{index + 1}</td>
+                    <td className="text-center">{sim.numero}</td>
+                    <td className="text-center">{sim.operador}</td>
+                    <td className='text-center'>{sim.estado}</td>
+                    <td className='uppercase text-center'>{sim.serial}</td>
+                    <td className='text-center'>Salen</td>
                   </tr>))
                 : <tr><td colSpan='5' className='text-center'>No Se Realizó Salida De Simcards</td></tr>
             }
--- src/pages/Movimientos/VerMovimientos.jsx
+++ src/pages/Movimientos/VerMovimientos.jsx
@@ -1,12 +1,13 @@
+import { useNavigate } from 'react-router-dom'
 import { useFilterMovimientos } from '../../hooks/useFilters.js'
 import { getMovimientos } from '../../services/FetchItemsData.js'
 import { formatFecha } from '../../utils/funtions.js'
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 
 export function VerMovimientos ({ company }) {
   const [movimientos, setMovimientos] = useState([])
   const [sortOrder, setSortOrder] = useState('desc')
+  const navigate = useNavigate()
 
   useEffect(() => {
     getMovimientos(company)
@@ -34,29 +35,29 @@ export function VerMovimientos ({ company }) {
   return (
     <section className="w-full h-[93vh] overflow-auto">
 
-      <article className='p-2 bg-blue-700 mb-2'>
+      <article className='p-2 bg-blue-700 flex items-center justify-center'>
         <label className='pr-2 font-semibold'>Filtro: N° Incidente | Encargado : </label>
         <input type="text" value={searchMovimiento} onChange={ev => setSearchMovimiento(ev.target.value)} placeholder="Buscar Movimiento..."
           className="bg-slate-200 w-64 p-1 rounded-md border border-black" />
       </article>
 
       <table className='w-full'>
-        <thead >
+        <thead className='text-sm uppercase'>
           <tr>
-            <th className=" cursor-pointer hover:underline" onClick={toggleSortOrder}> N° Mov <span>{sortOrder === 'asc' ? '▼' : '▲'}</span></th>
-            <th> Fecha Mov</th>
-            <th> N° Incidente</th>
-            <th> Encargado</th>
-            <th> Origen</th>
-            <th> Destino</th>
-            <th> Cant Item Mov</th>
-            <th> Cant Sims Mov</th>
+            <th className="bg-blue-600 cursor-pointer hover:underline" onClick={toggleSortOrder}> N° Mov <span>{sortOrder === 'asc' ? '▼' : '▲'}</span></th>
+            <th className='bg-blue-600'> Fecha Mov</th>
+            <th className='bg-blue-600'> N° Incidente</th>
+            <th className='bg-blue-600'> Encargado</th>
+            <th className='bg-blue-600'> Origen</th>
+            <th className='bg-blue-600'> Destino</th>
+            <th className='bg-blue-600'> Cant Items Mov</th>
+            <th className='bg-blue-600'> Cant Sims Mov</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className='border-b border-t border-black'>
           {
             movimientos && sortedMovimientos.map(m => (
-              <tr key={m.movimientoId}>
+              <tr key={m.movimientoId} onClick={() => navigate(`/bodega/verMovimientos/detalle/${m._id}`)} className='cursor-pointer hover:bg-yellow-200'>
                 <td>{m.movimientoId}</td>
                 <td>{formatFecha(m.fecha)}</td>
                 <td>{m.incidente}</td>
