e6421b9f3f871d477e5c9f6deb8cd565fad594bf
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-05 10:47:04 -0500

    realizar el redirrecionamiento

--- src/pages/Movimientos/VerMovimientos.jsx
+++ src/pages/Movimientos/VerMovimientos.jsx
@@ -32,41 +32,46 @@ export function VerMovimientos ({ company }) {
   })
 
   return (
-    <section className="w-full h-[93vh] overflow-auto xl:text-xs 2xl:text-sm">
-      <article className='p-2 bg-blue-500 mb-2'>
+    <section className="w-full h-[93vh] overflow-auto">
+
+      <article className='p-2 bg-blue-700 mb-2'>
         <label className='pr-2 font-semibold'>Filtro: N° Incidente | Encargado : </label>
         <input type="text" value={searchMovimiento} onChange={ev => setSearchMovimiento(ev.target.value)} placeholder="Buscar Movimiento..."
-          className="bg-slate-100 w-64 p-2 rounded-md border border-black" />
-      </article>
-      <article className='grid grid-cols-8 py-2 gap-4 place-content-center  place-items-center border mx-2  border-black bg-gradient-to-b from-cyan-200 to-blue-300 font-semibold'>
-        <span className="overflow-ellipsis overflow-hidden cursor-pointer hover:underline"
-          onClick={toggleSortOrder}>N° Mov. Generado <span>{sortOrder === 'asc' ? '▼' : '▲'}</span></span>
-        <span className="overflow-ellipsis overflow-hidden">Fecha Movimiento:</span>
-        <span className="overflow-ellipsis overflow-hidden">N° Incidente:</span>
-        <span className="overflow-ellipsis overflow-hidden">Encargado:</span>
-        <span className="overflow-ellipsis overflow-hidden">Bodega Origen:</span>
-        <span className="overflow-ellipsis overflow-hidden">Bodega Destino:</span>
-        <span className="overflow-ellipsis overflow-hidden">Cantidad Items Movidos</span>
-        <span className="overflow-ellipsis overflow-hidden">Cantidad Sims Movidas</span>
+          className="bg-slate-200 w-64 p-1 rounded-md border border-black" />
       </article>
 
-      {
-        movimientos && sortedMovimientos.map(m => (
-          <Link to={`detalle/${m._id}`} key={m._id}>
-            <article
-              className="grid grid-cols-8 place-content-center text-center gap-4 items-stretch h-7 mx-2 border border-black mt-0.5 cursor-pointer hover:bg-yellow-100 bg-slate-200">
-              <span className="overflow-ellipsis overflow-hidden font-medium">{m.movimientoId}</span>
-              <span className="overflow-ellipsis overflow-hidden text-blue-800 font-medium">{formatFecha(m.fecha)}</span>
-              <span className="overflow-ellipsis overflow-hidden font-medium">{m.incidente}</span>
-              <span className="overflow-ellipsis overflow-hidden font-medium">{m.encargado}</span>
-              <span className="overflow-ellipsis overflow-hidden text-blue-800 font-medium">{m.bodegaOrigen?.nombre}</span>
-              <span className="overflow-ellipsis overflow-hidden text-blue-800 font-medium">{m.bodegaDestino?.nombre}</span>
-              <span className="overflow-ellipsis overflow-hidden font-medium">{m.items.length}</span>
-              <span className="overflow-ellipsis overflow-hidden font-medium">{m.simcards.entran.length + m.simcards.salen.length}</span>
-            </article>
-          </Link>
-        ))
-      }
+      <table className='w-full'>
+        <thead >
+          <tr>
+            <th className=" cursor-pointer hover:underline" onClick={toggleSortOrder}> N° Mov <span>{sortOrder === 'asc' ? '▼' : '▲'}</span></th>
+            <th> Fecha Mov</th>
+            <th> N° Incidente</th>
+            <th> Encargado</th>
+            <th> Origen</th>
+            <th> Destino</th>
+            <th> Cant Item Mov</th>
+            <th> Cant Sims Mov</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            movimientos && sortedMovimientos.map(m => (
+              <tr key={m.movimientoId}>
+                <td>{m.movimientoId}</td>
+                <td>{formatFecha(m.fecha)}</td>
+                <td>{m.incidente}</td>
+                <td>{m.encargado}</td>
+                <td>{m.bodegaOrigen?.nombre}</td>
+                <td>{m.bodegaDestino?.nombre}</td>
+                <td>{m.items.length}</td>
+                <td>{m.simcards.entran.length + m.simcards.salen.length}</td>
+              </tr>
+            )
+            )
+          }
+        </tbody>
+      </table>
+
     </section>
   )
 }
