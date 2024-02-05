89fbbb523002c477d5704ff219e912062bccb16a
llOrTeGall <IvanOrtega_97@hotmail.com>
2024-02-05 09:26:28 -0500

    estilos aletar cambiados

--- src/components/CambiarCompany.jsx
+++ src/components/CambiarCompany.jsx
@@ -3,15 +3,22 @@ export function CambiarCompany ({ defineCompany }) {
     <>
       <div className="absolute inset-0 bg-black opacity-60"></div>
       <section className="fixed inset-0 flex items-center justify-center z-50">
-        <article className='h-44 flex flex-col justify-center items-center bg-gray-400 rounded-md py-4 gap-4 px-32'>
-          <p className='pr-2 text-lg font-semibold bg-yellow-400 m-4 px-2 rounded-md'>Espera Antes De Continuar !!! </p>
-          <p className="text-base font-semibold">Tú cuenta se encuentra ligada a 2 Empresas</p>
-          <select className='p-2 border rounded-lg' onChange={ev => defineCompany(ev.target.value)}>
-            <option>Seleccione una empresa</option>
-            <option value='Servired'>Servired</option>
-            <option value='Multired'>Multired</option>
+
+        <article className="flex items-center flex-col gap-4 p-4 mb-4 text-sm text-yellow-400 bg-gray-800 rounded-xl" role="alert">
+          <svg className="flex-shrink-0 inline w-6 h-6 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          </svg>
+          <span className="sr-only">Info</span>
+          <div>
+            <span className="font-medium">Espera Antes De Continuar !</span> Tú cuenta se encuentra ligada a 2 Empresas
+          </div>
+          <select className='p-2 border rounded-lg text-blue-700 font-semibold cursor-pointer' onChange={ev => defineCompany(ev.target.value)}>
+            <option className="text-black font-semibold">Seleccione una empresa</option>
+            <option value='Servired' className="font-bold ">Servired</option>
+            <option value='Multired' className="font-bold ">Multired</option>
           </select>
         </article>
+
       </section>
     </>
   )
